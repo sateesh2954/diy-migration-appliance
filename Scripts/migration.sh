ba77e310ca02c7f54c778eb8abf17af88812fec5
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2022-08-05 20:48:34 +0530

    classic update

--- Scripts/migration.sh
+++ Scripts/migration.sh
@@ -346,13 +346,22 @@ create_custom_image (){
 			fi
 			echo $CUSTOM_IMAGE_NAME
 			echo $imgloc
-			ibmcloud is image-create $CUSTOM_IMAGE_NAME  --file $imgloc --os-name $osname --resource-group-id $resourcegroupid > $customimagedetailstmp
-        		imageid=`cat $customimagedetailstmp | grep ID | grep -v -E "image" | awk '{print $2}'`
-			retrycountcustom=$((retrycountcustom + 1))
-			loginfo "Creating Custom Image is in progress..."
-			ibmcloud target -r $REGION 
-			sleep 60
-			imgstatus=`ibmcloud is image $imageid | grep -e "Status  " | awk '{print $2}'`			
+			if [[ "$migratefrom" == "classic" ]];then
+				ibmcloud is image-create $CUSTOM_IMAGE_NAME.vhd  --file $imgloc --os-name $osname --resource-group-id $resourcegroupid > $customimagedetailstmp
+				imageid=`cat $customimagedetailstmp | grep ID | grep -v -E "image" | awk '{print $2}'`
+				retrycountcustom=$((retrycountcustom + 1))
+				loginfo "Creating Custom Image is in progress..."
+				ibmcloud target -r $REGION 
+				sleep 60
+				imgstatus=`ibmcloud is image $imageid | grep -e "Status  " | awk '{print $2}'`
+			else 
+				ibmcloud is image-create $CUSTOM_IMAGE_NAME  --file $imgloc --os-name $osname --resource-group-id $resourcegroupid > $customimagedetailstmp
+        			imageid=`cat $customimagedetailstmp | grep ID | grep -v -E "image" | awk '{print $2}'`
+				retrycountcustom=$((retrycountcustom + 1))
+				loginfo "Creating Custom Image is in progress..."
+				ibmcloud target -r $REGION 
+				sleep 60
+				imgstatus=`ibmcloud is image $imageid | grep -e "Status  " | awk '{print $2}'`			
 		done
     	else
 	    	imageid=$action
