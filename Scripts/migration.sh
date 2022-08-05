60c98b9f74601194f7b3907c3e3e710ffcc779cf
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2022-08-05 20:04:52 +0530

    #244 fix

--- Scripts/migration.sh
+++ Scripts/migration.sh
@@ -483,7 +483,6 @@ check_vsi (){
     	failure="true"
 	fi
 	if [[ $failure == false ]] && [[ "$vsistatus" == "running" ]]; then
-		vsiname=`ibmcloud is instance $vsiid --output json | .name | tr -d '"'`
 		loginfo "VSI Name :- $VSI_NAME"
 		loginfo "VSI ID :- $vsiid"
 		rm -rf $vsidetailstmp
